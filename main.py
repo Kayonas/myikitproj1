401583c951292faf4e27795af078356429d9cefc
Kayonas <pirimovkayonas@gmail.com>
2025-02-11 21:39:26 +0700

    my first commit

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help t https://www.jetbrains.com/help/pycharm/
+# See PyChrm help t https://www.jetbrains.com/help/pycharm/
